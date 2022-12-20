615263885f510ad35664cf0209add4d74d81a6ce
Sanjith Kumar <53137451+thesanjithkumar@users.noreply.github.com>
2022-12-20 11:35:53 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -4,3 +4,21 @@ python -m geektrust sample_input\input1.txt
 python -m geektrust sample_input\input2.txt
 python -m geektrust sample_input\input3.txt
 python -m geektrust sample_input\input4.txt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Rem author=Sanjith Kumar
